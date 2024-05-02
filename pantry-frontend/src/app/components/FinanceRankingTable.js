887040f55fb10f0550edb5ce394a457d9d17ca52
Porom <porom.kamal@gmail.com>
2024-05-01 23:30:46 -0400

    Fixed hidden itemse in the ranking

--- pantry-frontend/src/app/components/FinanceRankingTable.js
+++ pantry-frontend/src/app/components/FinanceRankingTable.js
@@ -5,8 +5,8 @@ function FinanceRankingTable({data}) {
 
   return (
     <div className='flex flex-col w-4/12 max-h-full overflow-hidden'>
-      <div className='w-10/12 text-2xl font-light w-full'>Purchase Ranking</div>
-      <Table isHeaderSticky className='w-10/12 max-h-full m-1' aria-label="Example empty table">
+      <div className='w-10/12 text-2xl font-light'>Purchase Ranking</div>
+      <Table isHeaderSticky className='w-10/12 h-full pb-10 m-1' aria-label="Example empty table">
         <TableHeader>
           <TableColumn className='text-center w-4/12'>Name</TableColumn>
           <TableColumn className='text-center w-4/12'>Money Spent ($)</TableColumn>
